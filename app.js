40f5e0e99b64ef8976011f3e2fbfb2e162043a53
Chelsie Ty <chelsiety@gmail.com>
2023-02-24 00:53:56 +0800

    Add mouseover event listener to show bg color (black) when you hover over the div grid cells

--- app.js
+++ app.js
@@ -2,17 +2,35 @@
 const grid = document.querySelector(".grid-container");
 const gridSlider = document.querySelector("#grid-size-slider");
 const gridSizeDisplayLabel = document.querySelector('label[for="grid-size"]');
+const colorModeButtons = document.querySelectorAll(".color-mode-button");
 
+let defaultColorMode = "custom-color"; // default
 
 
 createGrid(16, 16);
 
 
-// Event listeners
+
+// EVENT LISTENERS
 gridSlider.addEventListener('input', displayGridSize)
 gridSlider.addEventListener('change', createGrid)
 
-// Functions
+// Mouse state tracker
+document.body.addEventListener('mousedown', () => {
+    mouseDown = true;
+})
+document.body.addEventListener('mouseup', ()=> {
+    mouseDown = false;
+})
+
+
+colorModeButtons.forEach(colorButton => colorButton.addEventListener('click', getColorMode));
+
+
+
+
+// FUNCTIONS
+
 function createGrid(rows, cols) {
     rows = gridSlider.value;
     cols = gridSlider.value;
@@ -20,18 +38,44 @@ function createGrid(rows, cols) {
     grid.style.setProperty('--grid-rowNum', rows);
     grid.style.setProperty('--grid-colNum', cols);
 
+    // Insert Child Divs for grid
     for (let i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
         cell.classList.add("grid-cell");
         grid.appendChild(cell);
+
+
+        cell.addEventListener('mouseover', (e) => e.target.style.backgroundColor = "black" )
     };
+
+
+
+
+
 };
-
-
 
 function displayGridSize() {
     gridSizeDisplayLabel.textContent = `Grid size: ${gridSlider.value} x ${gridSlider.value}`
 }
 
 
+function getColorMode(buttonClickedEvent){
+
+    const colorMode = buttonClickedEvent.target.value;
+
+    // Draw when mouse is held down (mousedown)
+    if (colorMode === 'custom-color'){
+        console.log("get custom color function")
+    }
+    else if (colorMode === 'random-color'){
+        console.log("get random-color function")
+    }
+    else if (colorMode === 'rainbow'){
+        console.log("get rainbow function")
+    }
+    else if (colorMode === 'grayscale'){
+        console.log("get grayscale function")
+    }
+    console.log(colorMode);
+}
 
